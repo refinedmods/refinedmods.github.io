448a209eb4623cac6ba7321e6f68a73ecb2b60f2
raoulvdberge <raoulvdberge@gmail.com>
2023-12-24 00:50:49 +0100

    Add utility to build locally

---
+++ build-local.sh
@@ -0,0 +1,10 @@
+refinedSitesVersion="0.1.2"
+
+if [ ! -f "refinedsites-$refinedSitesVersion-all.jar" ]; then
+  echo "downloading Refined Sites $refinedSitesVersion"
+  wget "https://github.com/refinedmods/refinedsites/releases/download/v$refinedSitesVersion/refinedsites-$refinedSitesVersion-all.jar"
+fi
+
+export "$(cat .env | xargs)"
+
+java -jar refinedsites-$refinedSitesVersion-all.jar .
