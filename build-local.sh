5b44648700df9d80472834d2307bc7bd5d947fc9
raoulvdberge <raoulvdberge@gmail.com>
2025-03-26 15:04:31 +0100

    Update for RS2

--- build-local.sh
+++ build-local.sh
@@ -5,7 +5,7 @@ rm -rf dist/
 rm -rf output/
 
 if [ ! -f "refinedsites-$refinedSitesVersion-all.jar" ]; then
-  echo "downloading Refined Sites $refinedSitesVersion"
+  echo "Downloading Refined Sites $refinedSitesVersion"
   wget "https://github.com/refinedmods/refinedsites/releases/download/v$refinedSitesVersion/refinedsites-$refinedSitesVersion-all.jar"
 fi
 
